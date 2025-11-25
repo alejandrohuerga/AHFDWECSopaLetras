0b4b4de5eee97c50f2cea65577ad0e7cbf871e15
alejandro.dev <alejandro.dev@gmail.com>
2025-11-25 13:55:31 +0100

    feat: :zap: CORRECCIÓN MÉTODO SELECCIONAR PALABRA
    Se ha corregido la seleccion si la direccion es incorrecta

--- webroot/js/funcionalidad.js
+++ webroot/js/funcionalidad.js
@@ -25,6 +25,7 @@ export function miFuncionClick(event) {
 }
 
 
+// Función que selecciona la palabra segun las celdas a las que hagamos click.
 
 function seleccionarPalabra(inicio, fin) {
     const filaInicio = inicio.parentElement.rowIndex;
@@ -32,6 +33,7 @@ function seleccionarPalabra(inicio, fin) {
 
     const filaFin = fin.parentElement.rowIndex;
     const colFin = fin.cellIndex;
+    console.log(filaInicio,colInicio,filaFin,colFin);
 
     const dx = Math.sign(colFin - colInicio);
     const dy = Math.sign(filaFin - filaInicio);
@@ -40,13 +42,31 @@ function seleccionarPalabra(inicio, fin) {
     let y = filaInicio;
 
     const tabla = inicio.closest("table");
-    while (true) {
-        tabla.rows[y].cells[x].classList.add("seleccionada");
-        if (x === colFin && y === filaFin) break;
-        x += dx;
-        y += dy;
-    }
+    //const tabla=document.getElementsByTagName("table")[0];
+
+        console.log(Math.abs(colFin - colInicio),Math.abs(filaFin - filaInicio));
+
+        if ((colInicio==colFin)||(filaFin==filaInicio)||(Math.abs(colFin - colInicio)==Math.abs(filaFin - filaInicio))){
+            while (true) { // Quitar el while true por otra estructura de control.
+                tabla.rows[y].cells[x].classList.add("seleccionada");
+                if (x === colFin && y === filaFin) break;
+                x += dx;
+                y += dy;
+            }
+        }else{
+            inicio.classList.remove("seleccionada");
+        }
 }
+
+
+
+
+
+
+
+
+
+
 
 
 
